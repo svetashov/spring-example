505502f9210464341f4844e9fd22b2e57fa2e43d
svetashov <grandais1@gmail.com>
2020-10-05 18:19:25 +0300

    first commit

---
+++ src/main/java/leaders/tech/join/controller/AboutController.java
@@ -0,0 +1,37 @@
+package leaders.tech.join.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@RequestMapping("about")
+@EnableSwagger2
+public class AboutController {
+
+    @Value("${application.name}")
+    private String applicationName;
+
+    @Value("${build.version}")
+    private String buildVersion;
+
+    @Value("${build.timestamp}")
+    private String buildTimestamp;
+
+    private Map<String, String> _about = new HashMap<>();
+
+    @GetMapping
+    public Map<String, String> getAbout() {
+        _about.put("name", applicationName);
+        _about.put("version", buildVersion);
+
+        return _about;
+    }
+}
---
+++ src/test/java/leaders/tech/join/JoinApplicationTests.java
@@ -0,0 +1,13 @@
+package leaders.tech.join;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JoinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
